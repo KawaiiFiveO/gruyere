5d0cc6a69234cfd3f8b770690d494d493c08c03e
OneHalf <49249543+KawaiiFiveO@users.noreply.github.com>
2024-11-03 21:33:43 -0800

    again

--- gruyere.py
+++ gruyere.py
@@ -569,7 +569,7 @@ class GruyereRequestHandler(BaseHTTPRequestHandler):
     The cookie is signed with a hash function.
     """
     if uid is None:
-      return (self.NULL_COOKIE, cookie_name + '=; path=/; Secure; HttpOnly; SameSite=None; Max-Age=3600')
+      return (self.NULL_COOKIE, cookie_name + '=; path=/; Secure; SameSite=None; Max-Age=3600')
     database = self._GetDatabase()
     profile = database[uid]
     if profile.get('is_author', False):
@@ -586,7 +586,7 @@ class GruyereRequestHandler(BaseHTTPRequestHandler):
 
     # global cookie_secret; only use positive hash values
     h_data = str(hash(cookie_secret + c_data) & 0x7FFFFFF)
-    c_text = f'{cookie_name}={h_data}|{c_data}; path=/; Secure; HttpOnly; SameSite=None; Max-Age=3600'
+    c_text = f'{cookie_name}={h_data}|{c_data}; path=/; Secure; SameSite=None; Max-Age=3600'
     return (c, c_text)
 
   def _GetCookie(self, cookie_name):
@@ -877,14 +877,13 @@ class WSGIHandler(GruyereRequestHandler):
 
     def send_response(self, code):
         """Send an HTTP response."""
+        # Set-Cookie header if a new cookie is created
+        if hasattr(self, 'new_cookie_text'):
+            self.headers['Set-Cookie'] = self.new_cookie_text
         # Call the start_response callback with headers as a list of tuples
-        response_headers = list(self.headers.items())
-        
-        # Add Access-Control-Expose-Headers to allow Set-Cookie to be accessed
-        response_headers.append(('Access-Control-Expose-Headers', 'Set-Cookie'))
+        self.start_response(f"{code} {self.responses[code][0]}", list(self.headers.items()))
     
-        self.start_response(f"{code} {self.responses[code][0]}", response_headers)
-    
+
     def end_headers(self):
         """End the headers."""
         pass
